09398753b3cb6897411a05f23c63a9d02fa40035
pourya-asrzad <poriaasrzad@gmail.com>
2024-01-08 23:13:44 +0330

    feat : static data added

---
+++ app/static/categories.js
@@ -0,0 +1,26 @@
+const categories = [
+    {
+        name: "Work",
+        id: 1,
+        color: "#42aafd"
+    }, {
+        name: "Personal",
+        id: 2,
+        color: "green"
+    }
+    , {
+        name: "Shopping",
+        id: 3,
+        color: "#cd42fd"
+    }
+    , {
+        name: "Health",
+        id: 4,
+        color: "#ee2375"
+    }, {
+        name: "Other",
+        id: 5,
+        color: "blue"
+    }
+]
+
