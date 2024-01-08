fa2c14e07f91931bd7a6aec701cfc096ed029e9c
pourya-asrzad <poriaasrzad@gmail.com>
2024-01-08 22:30:51 +0330

    feat : todo rendered

---
+++ app/todoData.js
@@ -0,0 +1,71 @@
+export const todoData = [
+    {
+        id: 1,
+        title: "cooking",
+        date: 5346354634,
+        startTime: {
+            label: "8:00",
+            position: 1
+        },
+        endTime: {
+            label: "8:30",
+            position: 2
+        },
+        category: 1,
+        description: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odio",
+        done: true,
+        createdDate: 23545545,
+        editedDate: 58756874
+    }, {
+        id: 1,
+        title: "cooking",
+        date: 5346354634,
+        startTime: {
+            label: "9:00",
+            position: 3
+        },
+        endTime: {
+            label: "9:30",
+            position: 4
+        },
+        category: 1,
+        description: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odio",
+        done: true,
+        createdDate: 23545545,
+        editedDate: 58756874
+    }, {
+        id: 1,
+        title: "golam",
+        date: 5346354634,
+        startTime: {
+            label: "8:30",
+            position: 2
+        },
+        endTime: {
+            label: "9:00",
+            position: 3
+        },
+        category: 1,
+        description: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odio",
+        done: true,
+        createdDate: 23545545,
+        editedDate: 58756874
+    }, {
+        id: 1,
+        title: "go out",
+        date: 5346354634,
+        startTime: {
+            label: "11:00",
+            position: 7
+        },
+        endTime: {
+            label: "11:30",
+            position: 8
+        },
+        category: 1,
+        description: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, odio",
+        done: true,
+        createdDate: 23545545,
+        editedDate: 58756874
+    }
+]
